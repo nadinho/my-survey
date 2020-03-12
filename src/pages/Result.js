11d2106ad32111d73ad412437d9df04adcc0fdea
nadinho <rockt2935@gmail.com>
2020-03-12 18:13:44 +0100

    fix PascalCase for the hundredth and hopefully last time :'(

--- src/pages/Result.js
+++ src/pages/Result.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useParams } from "react";
 // import { Link } from "react-router-dom";
-import Container from "../components/container";
+import Container from "../components/Container";
 // import CreateButton from "../components/CreateButton";
 
 const POLLS_API_URL =
