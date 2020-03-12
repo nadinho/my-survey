06e88db641fb31487e4b35caf1bf1ce6cf4c5d28
nadinho <rockt2935@gmail.com>
2020-03-12 13:59:47 +0100

    delete unused files

--- src/components/QuestionInput.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "./QuestionInput.css";
-
-export default function AskQuestion(props) {
-  return <input type="text" value = {question}
-  onChange = {event () => setQuestion(event.target.value)}class="input" placeholder="Ask your question"></input>;
-}
