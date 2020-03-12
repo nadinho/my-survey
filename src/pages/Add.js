4aef10152c6152068d561067935267392719b810
nadinho <rockt2935@gmail.com>
2020-03-12 15:32:43 +0100

    add POST request using fetch API

--- src/pages/Add.js
+++ src/pages/Add.js
@@ -5,11 +5,97 @@ import VoteButton from "../components/VoteButton";
 import AskQuestion from "../components/QuestionInput";
 
 function Add() {
+  const [question, setQuestion] = React.useState("");
+  const [answerOne, setAnswerOne] = React.useState("");
+  const [answerTwo, setAnswerTwo] = React.useState("");
+  const [answerThree, setAnswerThree] = React.useState("");
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const poll = {
+      question: question,
+      answerOne: answerOne,
+      answerTwo: answerTwo,
+      answerThree: answerThree
+    };
+    const response = await fetch("http://localhost:4000/polls", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8"
+      },
+      body: JSON.stringify(poll)
+    });
+    const createdPoll = await response.json();
+    alert(`Created new Poll with ${createdPoll.id}`);
+  }
+
   return (
-    <Container>
-      <p>Create your own poll</p>
-      <AskQuestion></AskQuestion>
-      <Link to="/vote">
+    <form className="form" onSubmit={handleSubmit}>
+      <Container>
+        <p>Create your own poll</p>
+
+        <input
+          value={question}
+          onChange={event => {
+            setQuestion(event.target.value);
+          }}
+          className="input"
+          placeholder="Ask your question"
+        ></input>
+
+        <p>Your answers</p>
+
+        <div className="answerOne">
+          <input
+            className="input"
+            placeholder="Answer 1"
+            value={answerOne}
+            onChange={event => {
+              setAnswerOne(event.target.value);
+            }}
+          ></input>
+          <label className="icons">
+            <i className="fas fa-plus-square"></i>
+
+            <i className="far fa-minus-square"></i>
+          </label>
+        </div>
+
+        <div className="answerTwo">
+          <input
+            className="input"
+            placeholder="Answer 2"
+            value={answerTwo}
+            onChange={event => {
+              setAnswerTwo(event.target.value);
+            }}
+          ></input>
+          <label className="icons">
+            <i className="fas fa-plus-square"></i>
+
+            <i className="far fa-minus-square"></i>
+          </label>
+        </div>
+
+        <div className="answerThree">
+          <input
+            className="input"
+            placeholder="Answer 3"
+            value={answerThree}
+            onChange={event => {
+              setAnswerThree(event.target.value);
+            }}
+          ></input>
+          <label className="icons">
+            <i className="fas fa-plus-square"></i>
+
+            <i className="far fa-minus-square"></i>
+          </label>
+        </div>
+
+        <Button>Vote</Button>
+        {/* <Link to="/vote">
         <VoteButton to="/vote" onClick={VoteButton}></VoteButton>
       </Link>
     </Container>
