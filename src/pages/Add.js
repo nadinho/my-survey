972e7107d7162ba982b7b79dd49a52eef42809b7
nadinho <rockt2935@gmail.com>
2020-03-12 12:36:34 +0100

    add form and logic to form

--- src/pages/Add.js
+++ src/pages/Add.js
@@ -2,49 +2,70 @@ import React from "react";
 // import { Link } from "react-router-dom";
 import Container from "../components/Container";
 // import VoteButton from "../components/VoteButton";
-import AskQuestion from "../components/QuestionInput";
+// import AskQuestion from "../components/QuestionInput";
 import Button from "../components/Button";
 import "./Add.css";
 
 function Add() {
+  const [question, SetQuestion] = React.useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    alert(JSON.stringify(poll));
+  }
+  const poll = {
+    question: question
+  };
   return (
-    <Container>
-      <p>Create your own poll</p>
-      <AskQuestion></AskQuestion>
-      <p>Your answers</p>
+    <form class="form" onSubmit={handleSubmit}>
+      <Container>
+        <p>Create your own poll</p>
 
-      <div className="answerOne">
-        <input class="input" placeholder="Answer 1"></input>
-        <label className="icons">
-          <i class="fas fa-plus-square"></i>
+        <input
+          type="text"
+          value={question}
+          onChange={event => {
+            SetQuestion(event.target.value);
+          }}
+          class="input"
+          placeholder="Ask your question"
+        ></input>
 
-          <i class="far fa-minus-square"></i>
-        </label>
-      </div>
+        <p>Your answers</p>
 
-      <div className="answerTwo">
-        <input class="input" placeholder="Answer 2"></input>
-        <label className="icons">
-          <i class="fas fa-plus-square"></i>
+        <div className="answerOne">
+          <input class="input" placeholder="Answer 1"></input>
+          <label className="icons">
+            <i class="fas fa-plus-square"></i>
 
-          <i class="far fa-minus-square"></i>
-        </label>
-      </div>
+            <i class="far fa-minus-square"></i>
+          </label>
+        </div>
 
-      <div className="answerThree">
-        <input class="input" placeholder="Answer 3"></input>
-        <label className="icons">
-          <i class="fas fa-plus-square"></i>
+        <div className="answerTwo">
+          <input class="input" placeholder="Answer 2"></input>
+          <label className="icons">
+            <i class="fas fa-plus-square"></i>
 
-          <i class="far fa-minus-square"></i>
-        </label>
-      </div>
+            <i class="far fa-minus-square"></i>
+          </label>
+        </div>
 
-      <Button>Vote</Button>
-      {/* <Link to="/vote">
+        <div className="answerThree">
+          <input class="input" placeholder="Answer 3"></input>
+          <label className="icons">
+            <i class="fas fa-plus-square"></i>
+
+            <i class="far fa-minus-square"></i>
+          </label>
+        </div>
+
+        <Button>Vote</Button>
+        {/* <Link to="/vote">
         <VoteButton to="/vote" onClick={VoteButton}></VoteButton>
       </Link> */}
-    </Container>
+      </Container>
+    </form>
   );
 }
 
