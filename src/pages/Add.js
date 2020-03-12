33cee8a4d25efdd637a0a99c03ecafd13eb76bc4
nadinho <rockt2935@gmail.com>
2020-03-12 12:12:39 +0100

    add fontawesome icons

--- src/pages/Add.js
+++ src/pages/Add.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import Container from "../components/Container";
 // import VoteButton from "../components/VoteButton";
 import AskQuestion from "../components/QuestionInput";
 import Button from "../components/Button";
+import "./Add.css";
 
 function Add() {
   return (
@@ -11,9 +12,34 @@ function Add() {
       <p>Create your own poll</p>
       <AskQuestion></AskQuestion>
       <p>Your answers</p>
-      <input class="input" placeholder="Answer 1"></input>
-      <input class="input" placeholder="Answer 2"></input>
-      <input class="input" placeholder="Answer 3"></input>
+
+      <div className="answerOne">
+        <input class="input" placeholder="Answer 1"></input>
+        <label className="icons">
+          <i class="fas fa-plus-square"></i>
+
+          <i class="far fa-minus-square"></i>
+        </label>
+      </div>
+
+      <div className="answerTwo">
+        <input class="input" placeholder="Answer 2"></input>
+        <label className="icons">
+          <i class="fas fa-plus-square"></i>
+
+          <i class="far fa-minus-square"></i>
+        </label>
+      </div>
+
+      <div className="answerThree">
+        <input class="input" placeholder="Answer 3"></input>
+        <label className="icons">
+          <i class="fas fa-plus-square"></i>
+
+          <i class="far fa-minus-square"></i>
+        </label>
+      </div>
+
       <Button>Vote</Button>
       {/* <Link to="/vote">
         <VoteButton to="/vote" onClick={VoteButton}></VoteButton>
