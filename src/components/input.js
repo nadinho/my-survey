3d76a7ce487331fa88f06600d84facd79604970b
nadinho <rockt2935@gmail.com>
2020-03-12 09:42:26 +0100

    add input form

---
+++ src/components/input.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./input.css";
+
+export default function AskQuestion(props) {
+  return <input class="input" placeholder="Ask your question"></input>;
+}
--- src/pages/Add.js
+++ src/pages/Add.js
@@ -2,11 +2,13 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Container from "../components/container";
 import VoteButton from "../components/VoteButton";
+import AskQuestion from "../components/input";
 
 function Add() {
   return (
     <Container>
       <p>Create your own poll</p>
+      <AskQuestion></AskQuestion>
       <Link to="/vote">
         <VoteButton to="/vote" onClick={VoteButton}></VoteButton>
       </Link>
